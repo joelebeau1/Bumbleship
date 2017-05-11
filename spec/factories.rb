615b0d6168ed7996064821827010b17696c9dcf9
Simon Swartzman & Stacy Curry <pair+rtzm.stacurry@devbootcamp.com>
2017-05-11 13:02:26 -0500

    finish game, player, board, and ship factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,47 +1,29 @@
 FactoryGirl.define do
   factory :game do
     secret_key "123456"
-    factory :game_with_no_players do
-      transient do
-        player_count 0
-      end
-      after(:create) do |game, evaluator|
-        create_list(:players, evaluator.player_count, game: game)
-      end
-    end
-    factory :game_with_one_player do
-      transient do
-        player_count 1
-      end
-      after(:create) do |game, evaluator|
-        create_list(:players, evaluator.player_count, game: game)
-      end
-    end
-    factory :game_with_two_players do
-      transient do
-        player_count 2
-      end
-      after(:create) do |game, evaluator|
-        create_list(:players, evaluator.player_count, game: game)
-      end
-    end
   end
 
   factory :player do
-    name "fake_name"
+    sequence :name { |n| "player#{n}" }
   end
 
   factory :board do
-    transient do
-      ship_count 5
-    end
+    player
+    game
   end
 
   factory :cell do
+    # sequence(:x_coordinate) { |n|  }
+    # sequence(:y_coordinate) {}
 
+    coordinates { "#{x_coordinate}#{y_coordinate}" }
+    guessed { [true, false].sample }
+    board
   end
 
   factory :ship do
-
+    sequence :name { |n| "ship#{n}" }
+    length { rand(2..5) }
+    board
   end
 end
