aa5bfed8d15b3f7009133f8fb61564e491720b79
Simon Swartzman <email": null>
2017-05-12 11:15:33 -0500

    Remove seed data in order to pass travis

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-joe = Player.find_or_create_by(name: 'Joe')
-stacy = Player.find_or_create_by(name: 'Stacy')
-
-game = Game.new(secret_key: '123456')
-joe_board = Board.new(player: joe, game: game)
-stacy_board = Board.new(player: stacy, game: game)
-
-Ship.create!(name: "test", length: 3, board: joe_board)
-Ship.create!(name: "test", length: 3, board: joe_board)
-Ship.create!(name: "test", length: 3, board: joe_board)
-Ship.create!(name: "test", length: 3, board: joe_board)
-Ship.create!(name: "test", length: 3, board: joe_board)
-
-Ship.create!(name: "test", length: 3, board: stacy_board)
-Ship.create!(name: "test", length: 3, board: stacy_board)
-Ship.create!(name: "test", length: 3, board: stacy_board)
-Ship.create!(name: "test", length: 3, board: stacy_board)
-Ship.create!(name: "test", length: 3, board: stacy_board)
-
-joe_board.save
-joe_board.generate_cells
-stacy_board.save
-stacy_board.generate_cells
-game.save
 
 
